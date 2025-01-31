a688293913bb70954a247e723d1eebf0563d81cb
asiakharada <wwwassabi1@gmail.com>
2025-01-31 23:19:26 +0300

    push

---
+++ hw-1.js
@@ -0,0 +1,66 @@
+// Task 1
+let a = 10
+alert(a)
+alert(a=20);
+
+// Task 2
+let iphone = 2007
+alert(iphone);
+
+// Task 3
+let jscreator = "Brendan Eich"
+alert(jscreator);
+
+// Task 4
+let q = 10
+let w = 2 
+alert(q + w)
+alert(q - w)
+alert(q * w)
+alert(q / w)
+
+// Task 5
+alert(w ** 5)
+
+// Task 6 
+a = 9
+b = 2
+alert(a + b)
+
+// Task 7 
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+// Task 8
+let age = prompt("Сколько вам лет?")
+alert(age);
+
+// Task 9 
+let user = {
+    name: "Ася",
+    age: 20,
+    isAdmin: true
+};
+
+// Task 10
+const name = prompt("Введите ваше имя")
+alert("Привет, {name}");
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ style.js
@@ -0,0 +1,41 @@
+// Task 1
+let password = "пароль";
+
+var password2 = "12345"
+var result = prompt("Введите пароль");
+if (result == password2){
+    alert("Пароль введен верно");
+} else {
+    alert("Пароль введен неправильно");
+}
+   
+// Task 2 
+let c = -3;
+if (c > 0 && c < 10) {
+    alert("Верно");
+} else {
+    alert("Неверно");
+}
+
+// Task 3
+let d = 13;
+let e = 113;
+
+if(d > 100 || e > 100){
+    alert("Верно");
+} else {
+    alert("Неверно");
+}
+
+// Task 4
+let a = 2;
+let b = 3;
+alert(Number(a) + Number(b));
+
+// Task 5
+ let monthNumber = 12;
+ if( monthNumber < 13){
+    alert("Верно");
+ } else{
+    alert("Неверно");
+ }
